70ea27b41dc71a3796dc72e102a2912143b4c75e
zzh1996 <zzh1996@mail.ustc.edu.cn>
2016-04-02 21:31:27 +0800

    add to github

---
+++ GPA Calc/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace GPA_Calc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleRight;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(636, 506);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            this.dataGridView1.ColumnSortModeChanged += new System.Windows.Forms.DataGridViewColumnEventHandler(this.dataGridView1_ColumnSortModeChanged);
+            this.dataGridView1.Sorted += new System.EventHandler(this.dataGridView1_Sorted);
+            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 524);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "粘贴成绩";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 550);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 524);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(102, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "粘贴选课结果";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 669);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "GPA计算器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+
+    }
+}
+
---
+++ GPA Calc/Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GPA_Calc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] headers = { "学期", "课程", "成绩", "学分", "绩点" };
+            foreach (string s in headers)
+            {
+                DataGridViewColumn c = new DataGridViewTextBoxColumn();
+                c.HeaderText = s;
+                dataGridView1.Columns.Add(c);
+            }
+            DataGridViewColumn c2 = new DataGridViewCheckBoxColumn();
+            c2.HeaderText = "成绩已出";
+            dataGridView1.Columns.Add(c2);
+            dataGridView1.Columns[4].ReadOnly = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = Clipboard.GetText();
+            string[] lines = data.Split(Environment.NewLine.ToCharArray());
+            foreach (string line in lines)
+            {
+                string[] cols = line.Split('\t');
+                if (cols.Length < 7 || cols[0] == "学期") continue;
+                if (exist(cols[0], cols[2])) continue;
+                int index = dataGridView1.Rows.Add();
+                DataGridViewRow r = dataGridView1.Rows[index];
+                r.HeaderCell.Value = dataGridView1.RowCount.ToString();
+                r.Cells[0].Value = cols[0];
+                r.Cells[1].Value = cols[2];
+                r.Cells[2].Value = cols[4];
+                r.Cells[3].Value = cols[6];
+                if (!(new string[] { "放弃","缓考","缓修","暂缺"}).Contains(cols[4]))
+                    r.Cells[5].Value = true;
+            }
+        }
+
+        private bool exist(string term, string course)
+        {
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                if ((string)r.Cells[0].Value == term && (string)r.Cells[1].Value == course) return true;
+            }
+            return false;
+        }
+
+        private void dataGridView1_ColumnSortModeChanged(object sender, DataGridViewColumnEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_Sorted(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                r.HeaderCell.Value = (r.Index + 1).ToString();
+            }
+        }
+
+        private double GetGPA(string grade)
+        {
+            double g;
+            if (Double.TryParse(grade, out g))
+            {
+                if (g >= 95) grade = "A+";
+                else if (g >= 90) grade = "A";
+                else if (g >= 85) grade = "A-";
+                else if (g >= 82) grade = "B+";
+                else if (g >= 78) grade = "B";
+                else if (g >= 75) grade = "B-";
+                else if (g >= 72) grade = "C+";
+                else if (g >= 68) grade = "C";
+                else if (g >= 65) grade = "C-";
+                else if (g >= 64) grade = "D+";
+                else if (g >= 61) grade = "D";
+                else if (g >= 60) grade = "D-";
+            }
+            switch (grade)
+            {
+                case "通过": return 4.3;
+                case "A+": return 4.3;
+                case "A": return 4;
+                case "A-": return 3.7;
+                case "B+": return 3.3;
+                case "B": return 3;
+                case "B-": return 2.7;
+                case "C+": return 2.3;
+                case "C": return 2;
+                case "C-": return 1.7;
+                case "D+": return 1.5;
+                case "D": return 1.3;
+                case "D-": return 1;
+                default: return 0;
+            }
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void dataGridView1_Paint(object sender, PaintEventArgs e)
+        {
+            /*foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                if ((bool)r.Cells[5].EditedFormattedValue)
+                {
+                    r.DefaultCellStyle.BackColor = Color.White;
+                }
+                else
+                {
+                    r.DefaultCellStyle.BackColor = Color.Gray;
+                }
+            }*/
+            ReCalc();
+        }
+
+        private void ReCalc()
+        {
+            double TotalPoint = 0, GPAPoint = 0, GotPoint = 0, Sum = 0,Point,GP;
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                GP=GetGPA((string)r.Cells[2].Value);
+                r.Cells[4].Value = GP.ToString();
+                if((bool)r.Cells[5].EditedFormattedValue)
+                {
+                    Point = Double.Parse((string)r.Cells[3].Value);
+                    TotalPoint += Point;
+                    string g = (string)r.Cells[2].Value;
+                    if (g != "通过" && g != "不通过")
+                    {
+                        GPAPoint += Point;
+                        Sum += GP * Point;
+                        //r.DefaultCellStyle.BackColor = Color.Red;
+                    }
+                    if (GP > 0)
+                    {
+                        GotPoint += Point;
+                    }
+                }
+            }
+            double GPA = Sum / GPAPoint;
+            label1.Text =
+                "当前GPA：" + (GPAPoint==0?"-":GPA.ToString()) + Environment.NewLine +
+                "已获学分：" + GotPoint.ToString() + " / " + TotalPoint.ToString();
+
+            foreach (DataGridViewRow r in dataGridView1.Rows)
+            {
+                if ((bool)r.Cells[5].EditedFormattedValue)
+                {
+                    if ((string)r.Cells[2].Value != "通过" && (string)r.Cells[2].Value != "不通过")
+                    {
+                        if (Double.Parse((string)r.Cells[4].Value) >= GPA)
+                        {
+                            r.DefaultCellStyle.BackColor = Color.Red;
+                        }
+                        else
+                        {
+                            r.DefaultCellStyle.BackColor = Color.Green;
+                        }
+                    }
+                    else
+                    {
+                        r.DefaultCellStyle.BackColor = Color.White;
+                    }
+                }
+                else
+                {
+                    r.DefaultCellStyle.BackColor = Color.Gray;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string data = Clipboard.GetText();
+            string[] lines = data.Split(Environment.NewLine.ToCharArray());
+            foreach (string line in lines)
+            {
+                /*string[] cols = line.Split('\t');
+                if (cols.Length < 7 || cols[0] == "学期") continue;
+                if (exist(cols[0], cols[2])) continue;
+                int index = dataGridView1.Rows.Add();
+                DataGridViewRow r = dataGridView1.Rows[index];
+                r.HeaderCell.Value = dataGridView1.RowCount.ToString();
+                r.Cells[0].Value = cols[0];
+                r.Cells[1].Value = cols[2];
+                r.Cells[2].Value = cols[4];
+                r.Cells[3].Value = cols[6];
+                if (!(new string[] { "放弃", "缓考", "缓修", "暂缺" }).Contains(cols[4]))
+                    r.Cells[5].Value = true;*/
+            }
+        }
+    }
+}
